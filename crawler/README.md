8a137e95392fca051244559e4cedd5ca6f7afb97
vianho <60379375+vianho@users.noreply.github.com>
2026-03-24 11:50:46 +0800

    chore: added assignment instructions

---
+++ crawler/README.md
@@ -0,0 +1,4 @@
+# Crawler using PageRank
+
+- Write a program/software that takes a small directed web graph (represented as a dictionary of URLs and their outlinks) and precomputed PageRank scores, then returns the top-k URLs to crawl based on authority, explaining why high-PageRank pages might yield higher-quality training data for generative AI models.
+- Propose one heuristic to find high-quality pages that permit crawling.
--- infra/scripts/cloud-init.sh
+++ infra/scripts/cloud-init.sh
@@ -22,3 +22,10 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 # 5. Export Storage Account Name to easily pull dataset
 echo "export STORAGE_ACCOUNT=__STORAGE_ACCOUNT_NAME__" >> /etc/profile.d/azure_env.sh
 chmod +x /etc/profile.d/azure_env.sh
+
+# 6. Install code cli
+sudo apt update && sudo apt install software-properties-common apt-transport-https wget -y
+wget -O- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vscode.gpg
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/vscode stable main | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt update
+sudo apt install code -y
